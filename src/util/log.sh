493c873b33640240fa371a4de51bc90e2517047d
Dmitrii Chizha <me@filaco.dev>
2025-11-13 16:41:37 +0300

    fix(logging): remove logs temporary

--- src/util/log.sh
+++ src/util/log.sh
@@ -2,7 +2,7 @@ log::init() {
   local log_file="$CJDEV_LOG_DIR"/latest.log
   local log_file_desc=3
   mkdir -p "$CJDEV_LOG_DIR"
-  exec "$log_file_desc" >"$log_file"
+  #exec "$log_file_desc" >"$log_file"
 }
 
 log::with_date() {
