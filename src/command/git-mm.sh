f237c1dc64c9b307915351aff274afda5ef6d7c0
Dmitrii Chizha <chizha.dmitrii@huawei.com>
2025-11-19 13:11:08 +0300

    feat(git-mm): change cd path on git-mm init

--- src/command/git-mm.sh
+++ src/command/git-mm.sh
@@ -119,7 +119,7 @@ git-mm::init::impl() {
 
     git-mm::config::set module."$path".branch "$1"
 
-    cd "$path"
+    cd "$CJDEV_HOST_WORKDIR"/"$path"
     git remote set-url origin "$(git-mm::config::get module."path".origin)"
     if ! git remote | grep -q "^upstream$"; then
       git remote add upstream "$(git-mm::config::get module."$path".upstream)"
